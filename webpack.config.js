4c7a0d0236b0509b780440860e695ba540936349
iagox86 <ron-git@skullsecurity.org>
2021-03-04 20:23:38 -0800

    Rename the output

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'terraria-research-tracker.js',
     library: 'terrariaResearchTracker',
   },
   resolve: {
