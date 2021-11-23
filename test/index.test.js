2e65b77b43ae66a847b1bdce5093ab17bcd4e6d1
angulz <angelfiguex@gmail.com>
2021-11-22 18:59:23 -0600

    Added 1.4.3.1 support.

--- test/index.test.js
+++ test/index.test.js
@@ -113,4 +113,9 @@ describe('Simple Test', () => {
     const data_researched = researched(fs.readFileSync(`${ TEST_PATH }/TestChar-1.4.3.plr`));
     assert.sameMembers(['IronPickaxe'], data_researched);
   });
+
+  it('Handles 1.4.3.1 correctly', function() {
+    const data_researched = researched(fs.readFileSync(`${ TEST_PATH }/TestChar-1.4.3.1.plr`));
+    assert.sameMembers(['IronPickaxe'], data_researched);
+  });
 });
